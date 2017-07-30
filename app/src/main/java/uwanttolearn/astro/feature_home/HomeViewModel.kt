623e7d4a5da6039e0f0c8843fd0352a26b1d45c1
Hafiz Waleed Hussain <hafiz.waleed@icarasia.com>
2017-07-30 12:33:22 +0800

    Tv-guide done

---
+++ app/src/main/java/uwanttolearn/astro/feature_home/HomeViewModel.kt
@@ -0,0 +1,82 @@
+package uwanttolearn.astro.feature_home
+
+import android.databinding.BaseObservable
+import android.databinding.ObservableInt
+import android.view.View
+import io.realm.*
+import uwanttolearn.astro.core.data.pojos.ChannelInfo
+import uwanttolearn.astro.core.data.source.ChannelsDataSource
+
+/**
+ * Created by waleed on 27/07/2017.
+ */
+class HomeViewModel(val channelsRepository: ChannelsDataSource,
+                    val view: HomeFragmentContract) : BaseObservable() {
+
+    private var allChannelsInfo: RealmResults<ChannelInfo>?
+
+    private var selectedSort: String = "channelStbNumber"
+
+    init {
+        channelsRepository.open()
+        allChannelsInfo = channelsRepository.getAllChannelsInfo(selectedSort)
+    }
+
+    val progressBarVisibilityHandler = ObservableInt(View.VISIBLE)
+
+    fun onCreateView() {
+
+        channelsRepository.addListener(allChannelsInfo, listener)
+        progressBarVisibilityHandler.set(View.GONE)
+        view.addData(allChannelsInfo)
+
+    }
+
+
+    private val listener = OrderedRealmCollectionChangeListener<RealmResults<ChannelInfo>> {
+        realmResults: RealmResults<ChannelInfo>, orderedCollectionChangeSet: OrderedCollectionChangeSet ->
+
+        if (orderedCollectionChangeSet == null)
+            return@OrderedRealmCollectionChangeListener
+
+        if (orderedCollectionChangeSet.insertionRanges.size > 0) {
+            view.addData(realmResults)
+            view.notifyDataChanged()
+        }
+
+
+        if (orderedCollectionChangeSet.changes.size > 0) {
+            for (index in orderedCollectionChangeSet.changes) {
+                view.updateRowOnPosition(index)
+            }
+        }
+
+
+    }
+
+    fun onDestroyView() {
+        channelsRepository.removeListener(allChannelsInfo, listener)
+        channelsRepository.close()
+
+    }
+
+    fun sortDataBy(title: CharSequence?) {
+        selectedSort = title.toString()
+        view.resetAdapter()
+        channelsRepository.reset()
+        allChannelsInfo = channelsRepository.getAllChannelsInfo(selectedSort)
+        onCreateView()
+    }
+
+    fun saveClick(pair: Pair<ChannelInfo, Boolean>) {
+        channelsRepository.executeTransaction(Realm.Transaction {
+            pair.first.isSave = pair.second
+            it.insertOrUpdate(pair.first)
+        })
+        if (selectedSort.equals("isSave"))
+            sortDataBy("isSave")
+
+    }
+
+
+}
